97eca3897221bf934cf3e7f1bae29f334ce9824d
Sneha Veerakumar <64252906+SnehaVeerakumar@users.noreply.github.com>
2021-03-14 11:14:05 +0530

    Update wsgi.py

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for sample project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
-
+from static_ranges import Ranges
 from django.core.wsgi import get_wsgi_application
-
+from dj_static import Cling, MediaCling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sample.settings')
 
-application = get_wsgi_application()
+application = Ranges(Cling(MediaCling(get_wsgi_application())))
